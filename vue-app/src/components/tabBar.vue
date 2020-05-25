97a9449e73e8fc5bd66c278a454137e452d41f79
sleepNick <1281148140@qq.com>
2020-05-25 22:05:35 +0800

    解决高度BUG

--- vue-app/src/components/tabBar.vue
+++ vue-app/src/components/tabBar.vue
@@ -4,35 +4,26 @@
     <router-view />
 
     <van-tabbar fixed placeholder route>
-      <van-tabbar-item replace to="/home" icon="wap-home">
-        首页
-      </van-tabbar-item>
-      <van-tabbar-item replace to="/classify" icon="new-arrival" :dot="true">
-        分类
-      </van-tabbar-item>
-      <van-tabbar-item replace to="/cart" icon="cart">
-        购物车
-      </van-tabbar-item>
-      <van-tabbar-item replace to="/discover" icon="hot" :dot="true">
-        发现
-      </van-tabbar-item>
-      <van-tabbar-item replace to="/my" icon="manager" :badge="5">
-        我的
-      </van-tabbar-item>
+      <van-tabbar-item replace to="/home" icon="wap-home">首页</van-tabbar-item>
+      <van-tabbar-item replace to="/classify" icon="new-arrival" :dot="true">分类</van-tabbar-item>
+      <van-tabbar-item replace to="/cart" icon="cart">购物车</van-tabbar-item>
+      <van-tabbar-item replace to="/discover" icon="hot" :dot="true">发现</van-tabbar-item>
+      <van-tabbar-item replace to="/my" icon="manager" :badge="5">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
 <script>
-  export default {
-    name: "tabBar",
-    data() {
-      return {
-      };
-    },
-    methods: {
-    },
-  };
+export default {
+  name: "tabBar",
+  data() {
+    return {};
+  },
+  methods: {}
+};
 </script>
 
 <style lang="scss" scoped>
+/deep/.van-tabbar__placeholder {
+  height: 54.6875px;
+}
 </style>
